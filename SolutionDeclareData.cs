550977772c7e49e4440aeedbc62080cef7c46fa6
错数螺线 <3253615697@qq.com>
2025-11-19 13:48:25 +0800

    添加项目文件。

---
+++ SolutionDeclareData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurnitureSolutionAnalyzer;
+
+internal readonly struct SolutionDeclareData(
+    string solutionName,
+    string categoryNameSpace, 
+    string rowIndex, 
+    string ingredientType,
+    string dustType)
+{
+    public string SolutionName { get; } = solutionName;
+    public string CategoryNameSpace { get; } = categoryNameSpace;
+    public string RowIndex { get; } = rowIndex;
+    public string IngredientType { get; } = ingredientType;
+    public string DustType { get; } = dustType;
+}
